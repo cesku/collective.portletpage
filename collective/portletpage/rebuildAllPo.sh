70dcc5b464423970120ba58381b4d06e17d36c33
Francesco Merlo <francesco.merlo@redturtle.it>
2022-09-16 16:14:51 +0200

    Cleanup translations

--- collective/portletpage/rebuildAllPo.sh
+++ collective/portletpage/rebuildAllPo.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 
 DOMAIN='collective.portletpage'
-I18NDUDE=i18ndude
+I18NDUDE=../../bin/i18ndude
 
 $I18NDUDE rebuild-pot --pot locales/${DOMAIN}.pot --create ${DOMAIN} .
 $I18NDUDE merge --pot locales/${DOMAIN}.pot --merge locales/${DOMAIN}-manual.pot
-$I18NDUDE sync --pot locales/${DOMAIN}.pot locales/*/LC_MESSAGES/${DOMAIN}.po
-
-DOMAIN=plone
-
-$I18NDUDE merge --pot locales/${DOMAIN}.pot --merge i18n/${DOMAIN}-manual.pot
 $I18NDUDE sync --pot locales/${DOMAIN}.pot locales/*/LC_MESSAGES/${DOMAIN}.po
